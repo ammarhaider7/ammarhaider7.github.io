823c63f17fed22d441ab1f74c7a8d25a398d784e
Ammar Haider <ammar.haider@experian.com>
2015-01-07 16:51:00 +0000

    Update AddSaveClear.js

--- AddSaveClear.js
+++ AddSaveClear.js
@@ -1,15 +1,23 @@
 $(function () {
-        $('#addAnother').click(function () {
-            if($('#jsSelVal').val() && $('.queryMethods textarea').val()) {
+    //Add another +
+    $('#addAnother').click(function () {
+        if ($('#jsSelVal').val()) {
             //Get the jQuery statement
             var jStatement = $('#scriptTags p').text();
-            //Append it after opening script tag
-            $('#scriptTags .tag').eq(0).after('<br><span>' + jStatement + ';' + '</span>');
+            //If no doc ready, append it after opening script tag, else append after doc ready
+//            if(
+//            $('#scriptTags .tag').eq(0).after('<br><span>' + jStatement + ';' + '</span>');
+            $('#dynamicJS').before('<br><span>' + jStatement + ';' + '</span>');
             //Clear live jQuery statement of text and classes
             $('#scriptTags p').text("").removeClass();
             //Remove methods
             $('.queryMethods').remove();
             $('#jsSelVal').val("");
-            }
-        });
+        }
+    });
+    //Add Doc ready
+    $('#copy-button').one("click", function () {
+        $('#scriptTags .tag').first().after('<span id="docReady">\n$(document).ready(function() {<span>');
+        $('#scriptTags .tag').last().before('<span>});\n<span>');
+    });
 });
