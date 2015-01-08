bb46ec25f5117d2798008474b9c26968a0ba7d24
Ammar Haider <ammar.haider@experian.com>
2015-01-08 18:13:51 +0000

    popovers

--- tooltipsInit.js
+++ tooltipsInit.js
@@ -1,3 +1,9 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+    //Initiate tooltips and popovers
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+    //Show Step 1 popover after 0.5s
+    setTimeout(function() {
+    $('#CSSfields form').popover('show');
+    },500);
+});
