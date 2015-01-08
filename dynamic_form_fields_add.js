69cfc3051cbf4a6d0094185647fe8a753d5570ed
Ammar Haider <ammar.haider@experian.com>
2015-01-08 18:12:25 +0000

    Update dynamic_form_fields_add.js

--- dynamic_form_fields_add.js
+++ dynamic_form_fields_add.js
@@ -1,6 +1,7 @@
 $(function () {
+
     $(document).on('click', '.btn-add, a', function (e) {
-//        e.preventDefault();
+        //        e.preventDefault();
         var controlForm = $('.controls form:first'),
             currentEntry = $(this).parents('.entry:first'),
             newEntry = $(currentEntry.clone()).appendTo(controlForm);
@@ -23,8 +24,21 @@ $(function () {
     });
     $("input, textarea").bind("keypress", function () {
         $('#outputFlash').addClass('blink');
+        $('#outputColumn').addClass('flash');
+        $('#CSSfields form').popover('destroy');
+        //            $('#outputHeader').popover('show');
+    });
+    //On the first keypress only, show the output popover
+    $("#CSSfields input").one("keypress", function() {
+        setTimeout(function() {
+        $('#outputColumn').tooltip('show');
+        setTimeout(function() {
+            $('#outputColumn').tooltip('destroy');
+        },3000);
+        },500);
     });
     $("input, textarea").blur(function () {
         $('#outputFlash').removeClass('blink');
+        //        $('#outputColumn').removeClass('flash');
     });
 });
