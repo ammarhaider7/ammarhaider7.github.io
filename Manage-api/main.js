2ec4c3d524e16fb86fe5258eb1584a8c7dfeff54
Ammar Haider <ammarhaider7@users.noreply.github.com>
2015-04-22 17:48:58 +0100

    Update main.js

--- Manage-api/main.js
+++ Manage-api/main.js
@@ -1,55 +1,51 @@
-(function() {
-    
-    //Run ajax on Dom ready
-    $(function() {
-        o.authAjax();
-    });
-    
-    var o = {
+//Run ajax on Dom ready
+$(function() {
+    o.authAjax();
+});
 
-        //CORS using jQuery
-        authAjax: function() {
+var o = {
+
+    //CORS using jQuery
+    authAjax: function() {
+            $.ajax({
+                  type: 'POST',
+                  url: '//manage-api.ensighten.com/auth/token',
+                  data: 'grant_type=password',
+                  contentType: 'application/x-www-form-urlencoded',
+
+                  headers: {
+                      "Authorization": "Basic " + btoa("experian" + ":" + "ammar.haider" + ":" + 'Haider26463.1!')
+                  },
+                  success: function(response) {
+                    // Here's where you handle a successful response.
+                      o.auth = response;
+                      o.tagPull(o.auth);
+                  },
+
+                  error: function() {
+                      console.log('Didn\'t work');
+                  }
+                })
+            },
+
+    //Get some deployments
+    tagPull: function(auth) {
                 $.ajax({
-                      type: 'POST',
-                      url: '//manage-api.ensighten.com/auth/token',
-                      data: 'grant_type=password',
+                      type: 'GET',
+                      url: '//manage-api.ensighten.com/manage/deployments?',
+                      data: 'name="UK SiteCatalyst Page Code"&spaceId="1501"',
                       contentType: 'application/x-www-form-urlencoded',
-
                       headers: {
-                          "Authorization": "Basic " + btoa("experian" + ":" + "ammar.haider" + ":" + 'Haider26463.1!')
+                          "Authorization": "Bearer " + auth.access_token,
+                          "Accept": "application/json"
                       },
                       success: function(response) {
-                        // Here's where you handle a successful response.
-                          o.auth = response;
-                          o.tagPull(o.auth);
+                          console.log(response);
                       },
-
                       error: function() {
                           console.log('Didn\'t work');
                       }
-                    })
-                },
-             
-        //Get some deployments
-        tagPull: function(auth) {
-                    $.ajax({
-                          type: 'GET',
-                          url: '//manage-api.ensighten.com/manage/deployments?name="UK SiteCatalyst Page Code"&spaceId="1501"}',
-                          data: 'grant_type=password',
-                          contentType: 'application/x-www-form-urlencoded',
-                          headers: {
-                              "Authorization": "Bearer " + auth.access_token,
-                              "Accept": "application/json"
-                          },
-                          success: function(response) {
-                              console.log(response);
-                          },
-                          error: function() {
-                              console.log('Didn\'t work');
-                          }
-                    })
-                }
+                })
+            }
 
-    }
-    
-})();
+}
