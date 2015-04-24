9a5af7835dc78a52289e0b9d1e4c355f346f94d1
Ammar Haider <ammarhaider7@users.noreply.github.com>
2015-04-24 13:55:03 +0100

    Update main.js

--- Manage-api/main.js
+++ Manage-api/main.js
@@ -76,7 +76,7 @@ var o = {
                       success: function(response) {
                           //console.log(response);
                           o.tag = response;
-                          $('.TagCode code').text(o.tag.code);
+                          $('.TagCode code').text(o.tag.);
                           $('.TagCode').show();
 
                       },
