25bb465f53221c610db93503e06f0d41b2285a7a
Ammar Haider <ammarhaider7@users.noreply.github.com>
2015-06-01 14:39:38 +0100

    Update tooltipsInit.js

--- tooltipsInit.js
+++ tooltipsInit.js
@@ -3,7 +3,7 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="popover"]').popover()
     //Show Step 1 popover after 0.5s
-    setTimeout(function() {
+/*    setTimeout(function() {
     $('#CSSfields form').popover('show');
-    },500);
+    },500);*/
 });
