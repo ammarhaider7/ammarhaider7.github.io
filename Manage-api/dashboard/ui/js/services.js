f8d41ca28385e6e32e4d9070e019ae920be3c45f
Ammar Haider <ammarhaider7@users.noreply.github.com>
2015-07-06 15:54:31 +0100

    Update services.js

--- Manage-api/dashboard/ui/js/services.js
+++ Manage-api/dashboard/ui/js/services.js
@@ -21,7 +21,7 @@ ensightenServices.factory('Signin', ['$resource', function($resource) {
   });
 }]);
 
-ensightenServices.factory('Auth', ['$resource',
+/*ensightenServices.factory('Auth', ['$resource',
   function ($resource) {
     return function(username, password) {
       return $resource('//manage-api.ensighten.com/auth/token', {}, {
@@ -37,4 +37,22 @@ ensightenServices.factory('Auth', ['$resource',
         }
       });
     }
+}]);*/
+
+ensightenServices.factory('Auth', ['$http', function ($http) {
+  return function(username, password) {
+    return {
+      connect: function() {
+        return $http({
+          method: "POST", 
+          url: "//manage-api.ensighten.com/auth/token",
+          data: 'grant_type=password',
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Basic " + btoa("experian" + ":" + username + ":" + password)
+          }
+        });
+      }
+    }
+  }
 }]);
