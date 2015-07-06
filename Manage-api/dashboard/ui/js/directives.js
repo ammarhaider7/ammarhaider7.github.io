08031b2c9fadce73930bdcfd03dab83b9e1e1454
Ammar Haider <ammarhaider7@users.noreply.github.com>
2015-07-06 11:58:39 +0100

    Create directives.js

---
+++ Manage-api/dashboard/ui/js/directives.js
@@ -0,0 +1,13 @@
+/*Directives*/
+var ensightenDirectives = angular.module('ensightenDirectives', []);
+
+ensightenDirectives.directive('highlightJs', ['$timeout', function($timeout) {
+  return {
+    link: function(scope, element, attributes) {
+      $timeout(function() { // You might need this timeout to be sure its run after DOM render.
+        hljs.highlightBlock(element[0]);
+      }, 100);
+
+    }
+  }
+}]);
