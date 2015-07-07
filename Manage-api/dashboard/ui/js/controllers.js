30aa1222255664413bb7475f20aef83854e9a9b9
Ammar Haider <ammarhaider7@users.noreply.github.com>
2015-07-07 18:56:41 +0100

    Update controllers.js

--- Manage-api/dashboard/ui/js/controllers.js
+++ Manage-api/dashboard/ui/js/controllers.js
@@ -43,34 +43,24 @@ ensightenControllers.controller('tagListCtrl', ['$scope', 'tokenFactory', 'Tags'
   }];
   $scope.orderProp = 'age';
   $scope.tableRun = false;
-$scope.getTags = function() {
-
-	var tags = tagFactory.getTags();
-	
-	if(tags) {
-		console.log(tags)
-		return $scope.tags = tags;
-
-	} else {
-
-		$scope.loader = true;
-		spaceFactory.setSpace($scope.selectedSpace.id);
-		console.log(tokenFactory);
-		console.log(spaceFactory);
-		Tags(tokenFactory.getToken(), spaceFactory.getSpace()).pull()
-			.success(function(tags) {
-				console.log(tags);
-				tagFactory.setTags(tags);
-				console.log(tagFactory);
-				return $scope.tags = tags;
-				$scope.loader = false;
-			})
-			.error(function() {
-				console.log("Error");
-				$scope.loader = false;
-			});				
-	}
-};
+$scope.getTags = function () {
+	$scope.loader = true;
+	spaceFactory.setSpace($scope.selectedSpace.id);
+	console.log(tokenFactory);
+	console.log(spaceFactory);
+	Tags(tokenFactory.getToken(), spaceFactory.getSpace()).pull()
+		.success(function(tags) {
+			console.log(tags);
+			tagFactory.setTags(tags);
+			console.log(tagFactory);
+			return $scope.tags = tags;
+			$scope.loader = false;
+		})
+		.error(function() {
+			console.log("Error");
+			$scope.loader = false;
+		});
+}
   $scope.orderProp = '-modifyDate';
   $scope.tableRun = true;
 }]);
