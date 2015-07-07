bdad1aa4824b64a732d84b9936ae89ca9b31aa30
Ammar Haider <ammarhaider7@users.noreply.github.com>
2015-07-07 18:25:25 +0100

    Update directives.js

--- Manage-api/dashboard/ui/js/directives.js
+++ Manage-api/dashboard/ui/js/directives.js
@@ -4,10 +4,9 @@ var ensightenDirectives = angular.module('ensightenDirectives', []);
 ensightenDirectives.directive('highlightJs', ['$browser', function($browser) {
   return {
     link: function(scope, element, attributes) {
-      $browser.notifyWhenNoOutstandingRequests(function() {
-        console.log('Im done!');
-	hljs.highlightBlock(element[0]);
-      });
+	$timeout(function () { // You might need this timeout to be sure its run after DOM render.
+    			hljs.highlightBlock(element[0]);
+        }, 300);
 
     }
   }
