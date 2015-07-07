eba53819e4af1ac32a66ac5b5647822e3ff97779
Ammar Haider <ammarhaider7@users.noreply.github.com>
2015-07-07 17:41:16 +0100

    Update directives.js

--- Manage-api/dashboard/ui/js/directives.js
+++ Manage-api/dashboard/ui/js/directives.js
@@ -4,7 +4,7 @@ var ensightenDirectives = angular.module('ensightenDirectives', []);
 ensightenDirectives.directive('highlightJs', ['$browser', function($browser) {
   return {
     link: function(scope, element, attributes) {
-      $routeChangeSuccess(function() {
+      $browser.notifyWhenNoOutstandingRequests(function() {
         console.log('Im done!');
 	hljs.highlightBlock(element[0]);
       });
