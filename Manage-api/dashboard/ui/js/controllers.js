0fe140507edbf3e85dfbfaa901c75815f20258e0
Ammar Haider <ammarhaider7@users.noreply.github.com>
2015-07-09 15:02:58 +0100

    Update controllers.js

--- Manage-api/dashboard/ui/js/controllers.js
+++ Manage-api/dashboard/ui/js/controllers.js
@@ -20,7 +20,7 @@ ensightenControllers.controller('tagListCtrl', ['$scope', 'tokenFactory', 'Tags'
   var matchSpaces = function(spaceName) {
   	var matchedSpace;
   	angular.forEach($scope.spaces, function (space) {
-  		console.log(space);
+  		// console.log(space);
   		if(space.name == spaceName) matchedSpace = space;
   	});
   	return matchedSpace;
@@ -28,23 +28,23 @@ ensightenControllers.controller('tagListCtrl', ['$scope', 'tokenFactory', 'Tags'
 if(tagFactory.getTags() != undefined) {
   $scope.tags = tagFactory.getTags();
   $scope.tableRun = true;
-  console.log(spaceFactory.getSpace());
-  console.log(spaceFactory.getSpace().name);
+  //console.log(spaceFactory.getSpace());
+  //console.log(spaceFactory.getSpace().name);
   $scope.selectedSpace = matchSpaces(spaceFactory.getSpace().name);
-  console.log($scope.selectedSpace);
+  //console.log($scope.selectedSpace);
 }
 $scope.getTags = function () {
 	$scope.tableRun = false;
 	$scope.loader = true;
-	console.log("name: " + $scope.selectedSpace.name + " id: " + $scope.selectedSpace.id);
+	// console.log("name: " + $scope.selectedSpace.name + " id: " + $scope.selectedSpace.id);
 	spaceFactory.setSpace($scope.selectedSpace.name, $scope.selectedSpace.id);
-	console.log(spaceFactory);
+	// console.log(spaceFactory);
 	Tags(tokenFactory.getToken(), spaceFactory.getSpaceId()).pull()
 		.success(function(tags) {
-			console.log(tags);
+			// console.log(tags);
 			tagFactory.setTags(tags);
-			console.log(tagFactory);
-			console.log("tagFactory.getTags(): " + tagFactory.getTags())
+			// console.log(tagFactory);
+			// console.log("tagFactory.getTags(): " + tagFactory.getTags())
 			$scope.loader = false;
 			$scope.tableRun = true;
 			return $scope.tags = tags;
@@ -66,62 +66,12 @@ ensightenControllers.controller('TagDetailCtrl', ['$scope', '$routeParams', 'tag
       }
     });
   });*/
-	console.log('$routeParams: ' + $routeParams.tagId);
-	console.log("tagFactory.getTagById($routeParams.tagId): " + tagFactory.getTagById($routeParams.tagId));
+	// console.log('$routeParams: ' + $routeParams.tagId);
+	// console.log("tagFactory.getTagById($routeParams.tagId): " + tagFactory.getTagById($routeParams.tagId));
   	$scope.tag = tagFactory.getTagById($routeParams.tagId);
   	$scope.tagId = $routeParams.tagId;
-  	console.log("$scope.tag: " + $scope.tag);
+  	// console.log("$scope.tag: " + $scope.tag);
 }]);
-
-//Sign in controller
-/*ensightenControllers.controller('signinCtrl', ['$scope', 'Signin', '$location', function($scope, Signin, $location) {
-  $scope.submit = function() {
-    Signin.query(function(data) {
-      angular.forEach(data, function(el) {
-        if (el.Username === $scope.user.name && el.Password === $scope.user.password) {
-          $location.path('/tags').replace();
-        }
-      });
-    });
-    $scope.invalid = true;
-  }
-}]);*/
-
-/*ensightenControllers.controller('signinCtrl', ['$scope', 'Auth', '$location',
-	function ($scope, Auth, $location) {
-		$scope.submit = function () {
-			Auth($scope.user.name, $scope.user.password).connect(
-				function() {
-					$location.path('/tags').replace();
-				},
-				function() {
-					$scope.invalid = true;
-				}
-			);
-		}
-	}
-]);*/
-
-/*ensightenControllers.controller('signinCtrl', ['$scope', '$http', '$location',
-	function ($scope, $http, $location) {
-		$scope.submit = function() {
-		$http({
-			method: "POST", 
-			url: "//manage-api.ensighten.com/auth/token",
-			data: 'grant_type=password',
-			headers: {
-				"Content-Type": "application/x-www-form-urlencoded",
-				"Authorization": "Basic " + btoa("experian" + ":" + $scope.user.name + ":" + $scope.user.password)
-			}
-		}).
-		success(function() {
-		  $location.path('/tags').replace();
-		}).
-		error(function() {
-			$scope.invalid = true;
-		});
-	}
-}]);*/
 
 ensightenControllers.controller('signinCtrl', ['$scope', '$location', 'tokenFactory', 'Auth',
 	function ($scope, $location, tokenFactory, Auth) {
