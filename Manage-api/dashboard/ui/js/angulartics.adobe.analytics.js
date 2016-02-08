79c72b76cead19a01125d04ec8f83c8758389618
Ammar Haider <ammarhaider7@users.noreply.github.com>
2016-02-08 16:01:08 +0000

    Update angulartics.adobe.analytics.js

--- Manage-api/dashboard/ui/js/angulartics.adobe.analytics.js
+++ Manage-api/dashboard/ui/js/angulartics.adobe.analytics.js
@@ -1,12 +1,4 @@
 !function(e) {
   "use strict";
-  e.module("angulartics.adobe.analytics", ["angulartics"]).config(["$analyticsProvider", function(e) {
-    e.settings.trackRelativePath = !0, e.registerPageTrackAdobe(function(e) {
-      window.s_ce && s_ce.t({
-        pageName: e
-      })
-    }), e.registerEventTrackAdobe(function(e) {
-      window.s_ce && e && ("DOWNLOAD" === e.toUpperCase() ? s_ce.tl(this, "d", e) : "EXIT" === e.toUpperCase() ? s_ce.tl(this, "e", e) : s_ce.tl(this, "o", e))
-    })
-  }])
+  e.module("angulartics.adobe.analytics", ["angulartics"]);
 }(angular);
